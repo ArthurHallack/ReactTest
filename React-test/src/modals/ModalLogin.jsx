f9fa30b301cb59a4a9caff2fa2be0672e177b081
ArthurHallack <ArthurHallack2@gmail.com>
2024-02-08 20:57:40 -0300

    yoshaa

--- React-test/src/modals/ModalLogin.jsx
+++ React-test/src/modals/ModalLogin.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function Modal () {
+function Modal ({display, setUserName, setSenha}) {
+
+    const value1 = useRef()
+    const value2 = useRef()
+
+    const CadastroCreate = (e)=>{
+        e.preventDefault()
+        const ValorLogin = value1.current.value
+        const ValorSenha = value2.current.value
+        setUserName(ValorLogin)
+        setSenha(ValorSenha)
+
+    }
+
     return(
-        <div>
+        <div id="ModalLogin" style={{display: display}}>
             <label>Usuario</label>
-            <input type="text" />
+            <input type="text" ref={value1}/>
             <label>Senha</label>
-            <input type="password" />
+            <input type="password" ref={value2}/>
+            <button type="button" onClick={CadastroCreate}>Cadastrar</button>
         </div>
     )
 }
+
+export default Modal
--- React-test/src/routes/Login.jsx
+++ React-test/src/routes/Login.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { useRef } from "react";
-
+import React, { useRef, useState, useHistory } from "react";
+import Modal from "../modals/ModalLogin";
 
 
 
@@ -8,11 +7,21 @@ function Login () {
 
     const Ref1 = useRef()
     const Ref2 = useRef()
-    const Value1 = Ref1.current.value
-    const Value2 = Ref2.current.value
+    const [ModalOpen, setModalOpen] = useState (false)
+    const [UserName, setUserName]= useState ("")
+    const [Senha, setSenha]= useState ("")
 
-    const Validation = (Value1, Value2)=>{
+    const Validation = (e)=>{
         
+        e.preventDefault()
+        const valueLogin = Ref1.current.value
+        const valorSenha = Ref2.current.value
+        console.log (UserName)
+        console.log(Senha)
+    }
+
+    const OpenModal = ()=>{
+        setModalOpen(true)
     }
 
     return(
@@ -24,8 +33,9 @@ function Login () {
                 <label className="LabelLogin">Senha</label>
                 <input type="password" className="InputLogin" ref={Ref2}/>
                 <button type="submit" className="LoginBTN" onClick={Validation}>Login</button>
-                <button type="button" className="LoginBTN" id="CadastroBTN">Cadastrar</button>
+                <button type="button" className="LoginBTN" id="CadastroBTN" onClick={OpenModal}>Cadastrar</button>
             </form>
+            <Modal display={ModalOpen ? "flex" : "none"} /> 
         </div>
     )
 }
