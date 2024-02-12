d089d6795b8c983eb42a0d4f64bfc87f0c62cf05
ArthurHallack <ArthurHallack2@gmail.com>
2024-02-12 20:12:41 -0300

    yosha

--- React-test/src/routes/Login.jsx
+++ React-test/src/routes/Login.jsx
@@ -1,17 +1,19 @@
-import React, { useRef, useEffect } from "react"
+import React, { useRef, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 function Login() {
     const Ref1 = useRef()
     const Ref2 = useRef()
     const navigate = useNavigate()
+    const [loginCadastro, setLoginCadastro] = useState("")
+    const [senhaCadastro, setSenhaCadastro] = useState("")
 
     useEffect(() => {
-
-        const loginCadastro = localStorage.getItem("loginCadastro")
-        const senhaCadastro = localStorage.getItem("senhaCadastro")
-        if (loginCadastro && senhaCadastro) {
-            console.log(senhaCadastro)
+        const login = localStorage.getItem("loginCadastro")
+        const senha = localStorage.getItem("senhaCadastro")
+        if (login && senha) {
+            setLoginCadastro(login)
+            setSenhaCadastro(senha)
         }
     }, [])
 
@@ -24,21 +26,30 @@ function Login() {
         e.preventDefault();
         const valueLogin = Ref1.current.value
         const valueSenha = Ref2.current.value
-
-        console.log("Login:", valueLogin)
-        console.log("Senha:", valueSenha)
+        if (valueLogin === loginCadastro && valueSenha === senhaCadastro) {
+            navigate('/Home')
+        } else {
+            alert("Login ou senha Incorretos")
+        }
     };
 
     return (
-        <div id="Container-Login">
+        <div id="Tela-Login">
             <form id="Form-Login">
-                <h1 id="H1Login">Login</h1>
-                <label htmlFor="" className="LabelLogin">Usuario</label>
-                <input type="text" className="InputLogin" ref={Ref1}/>
-                <label className="LabelLogin">Senha</label>
-                <input type="password" className="InputLogin" ref={Ref2}/>
-                <button type="submit" className="LoginBTN" onClick={Validation}>Login</button>
-                <button type="button" className="LoginBTN" id="CadastroBTN" onClick={Cadastrar}>Cadastre-se</button>
+                <img src="" alt="" id="img-Login"/>
+                <div id="Conteudo-Form">
+                    <div id="Conteudo-Form2">
+                    <h1 id="H1Login">Login</h1>
+                    <label htmlFor="" className="LabelLogin">Usuario</label>
+                    <input type="text" className="InputLogin" ref={Ref1}/>
+                    <label className="LabelLogin">Senha</label>
+                    <input type="password" className="InputLogin" ref={Ref2}/>
+                    <div id="DivBTN-Login">
+                        <button type="submit" className="LoginBTN" id="LOGINBTN" onClick={Validation}>Log in</button>
+                        <button type="button" className="LoginBTN" id="CadastroBTN" onClick={Cadastrar}>Cadastre-se</button>
+                    </div>
+                </div>
+                </div>
             </form> 
         </div>
     )
