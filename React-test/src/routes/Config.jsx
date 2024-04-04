8780fbd6c4c88a7d4a03649c5e9dc293e612691e
Arthur <ArthurHallack2@gmail.com>
2024-04-04 15:11:09 -0300

    Yoshaaa

--- React-test/src/routes/Config.jsx
+++ React-test/src/routes/Config.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import NavBar from "../components/Nav";
+import PuserImg from "../img/Puser.png";
 import "../css/routes.css/config.css"
 
 function Config () {
     return(
-        <div>
+        <div id="tela">
             <NavBar/>
-            <div>
-                <h1>Aqui será as configs</h1>
+            <div id="hud">
+                <div id="lateral">
+                    <section id="menu">
+                        <img src={PuserImg} alt="usuario" />
+                        <ul>
+                            <li><h1>Arthur Hallack</h1></li>
+                            <li><h2>Admin</h2></li>
+                            <li><p><i>integrasis account</i></p></li>
+                        </ul>
+                    </section> 
+                </div>               
             </div>
         </div>
     )
--- React-test/src/routes/Grafico.jsx
+++ React-test/src/routes/Grafico.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/Nav";
 import "../css/routes.css/grafico.css"
 
 function Grafico () {
+
     return(
         <div>
             <NavBar/>
