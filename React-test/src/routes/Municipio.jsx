e36ca9f874863e147da0563c511bf71c31a6da0f
ArthurHallack <ArthurHallack2@gmail.com>
2024-04-29 21:33:29 -0300

    Yoshaaa

--- React-test/src/routes/Municipio.jsx
+++ React-test/src/routes/Municipio.jsx
@@ -1,13 +1,51 @@
 import React from "react";
 import NavBar from "../components/Nav";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilter } from "@fortawesome/free-solid-svg-icons"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+
+import '../css/routes.css/municipio.css'
 
 function Municipio () {
     return(
-        <div>
+        <div id="TelaMunicipio">
             <NavBar/>
-            <div>
-                <h1>Tabela de Municipio</h1>
+            <div id="SecTop">
+                <h1>Municipio</h1>
+                <div id="SecTopBTN">
+                    <button><FontAwesomeIcon icon={faPlus} /></button>
+                    <button><FontAwesomeIcon icon={faFilter} /></button>
+                </div>
             </div>
+            <form action="" method="post" id="FormAdd">
+                <fieldset>
+                    <label htmlFor="" className="LabelForm">Pais</label>
+                    <input type="text" required className="InputForm"/>
+                </fieldset>
+                <fieldset>
+                    <label className="LabelForm">Municipio</label>
+                    <input type="text" className="InputForm" required/>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="" className="LabelForm">UF</label>
+                    <input type="text"  className="InputForm" required/>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="" className="LabelForm">DDD</label>
+                    <input type="number" className="InputForm" required/>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="" className="LabelForm">IBGE</label>
+                    <input type="number" className="InputForm" required/>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="" className="LabelForm">Situação</label>
+                    <div id="DivSit">
+                        <input type="checkbox" required />
+                        <p>Ativo</p>
+                    </div>
+                </fieldset>
+            </form>
         </div>
     )
 }
