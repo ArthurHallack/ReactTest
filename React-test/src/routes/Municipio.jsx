d8b44e611293528bb2d0c727ce45f0359b74be22
ArthurHallack <ArthurHallack2@gmail.com>
2024-05-07 21:59:02 -0300

    Yoshaaa

--- React-test/src/routes/Municipio.jsx
+++ React-test/src/routes/Municipio.jsx
@@ -11,12 +11,12 @@ import '../css/routes.css/municipio.css'
 function Municipio () {
 
     // Values INPUTS
-    const Ref1 = useRef();//Pais
-    const Ref2 = useRef();//Municipio
-    const Ref3 = useRef();//UF
-    const Ref4 = useRef();//DDD
-    const Ref5 = useRef();//IBGE
-    const Ref6 = useRef();//Situação
+    const Refe1 = useRef();//Pais
+    const Refe2 = useRef();//Municipio
+    const Refe3 = useRef();//UF
+    const Refe4 = useRef();//DDD
+    const Refe5 = useRef();//IBGE
+    const Refe6 = useRef();//Situação
 
     // ESTADOS DOS INPUTS
     const [PaisValue, setPaisValue] = useState('')
@@ -30,7 +30,7 @@ function Municipio () {
     const [FormVisivel, setFormVisivel] = useState (false)
 
     //FUNÇÕES
-
+    //ADD
     function ADD () {
         setFormVisivel(true)
         if(FormVisivel===true){
@@ -46,8 +46,29 @@ function Municipio () {
         window.document.getElementById('FormAdd').style.display='none'
         window.document.getElementById('SecTopBTN').style.display='flex'
         window.document.getElementById('Table-Municipio').style.display='flex'
+
+            Refe1.current.value = ''
+            Refe2.current.value = ''
+            Refe3.current.value = ''
+            Refe4.current.value = ''
+            Refe5.current.value = ''
+        
     }
 
+    //SAVE
+    function Salvar (event) {
+        event.preventDefault()
+        var dataNew = {
+            "id": 0,
+            "pais": Refe1.current.value,
+            "municipio": Refe2.current.value,
+            "uf": Refe3.current.value,
+            "ddd": Refe4.current.value,
+            "ibge": Refe5.current.value,
+            "situacao": Refe6.current.checked
+        }
+
+    }
 
     return(
         <div id="TelaMunicipio">
@@ -63,28 +84,28 @@ function Municipio () {
                 <div id="FormCampos">
                     <fieldset>
                         <label className="LabelForm">Pais</label>
-                        <input type="text" className="InputForm" id="InputPais" ref={Ref1} />
+                        <input type="text" className="InputForm" id="InputPais" ref={Refe1} />
                     </fieldset>
                     <fieldset>
                         <label className="LabelForm">Municipio</label>
-                        <input type="text" className="InputForm" id="InputMunicipio" ref={Ref2} />
+                        <input type="text" className="InputForm" id="InputMunicipio" ref={Refe2} />
                     </fieldset>
                     <fieldset>
                         <label className="LabelForm">UF</label>
-                        <input type="text"  className="InputForm" id="InputUf" ref={Ref3} />
+                        <input type="text"  className="InputForm" id="InputUf" ref={Refe3} />
                     </fieldset>
                     <fieldset>
                         <label className="LabelForm">DDD</label>
-                        <input type="number" className="InputForm" id="InputDDD" ref={Ref4} />
+                        <input type="number" className="InputForm" id="InputDDD" ref={Refe4} />
                     </fieldset>
                     <fieldset>
                         <label className="LabelForm">IBGE</label>
-                        <input type="number" className="InputForm" id="InputIBGE" ref={Ref5} />
+                        <input type="number" className="InputForm" id="InputIBGE" ref={Refe5} />
                     </fieldset>
                     <fieldset>
                         <label className="LabelForm">Situação</label>
                         <div id="DivSit">
-                            <input type="checkbox" ref={Ref6} />
+                            <input type="checkbox" ref={Refe6} />
                             <p>Ativo</p>
                         </div>
                         <div id="DivSitF">
@@ -97,7 +118,7 @@ function Municipio () {
                     </fieldset>
                 </div>
                 <div id="BtnFormAdd">
-                    <button id="BtnConfirmAdd"><FontAwesomeIcon icon={faCheck} /></button>
+                    <button id="BtnConfirmAdd" onClick={Salvar}><FontAwesomeIcon icon={faCheck} /></button>
                     <button id="BtnCloseAdd" onClick={CloseForm}><FontAwesomeIcon icon={faXmark}/></button>
                 </div>
                 <div id="BtnFormFilter">
