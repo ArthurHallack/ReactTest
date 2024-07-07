fc904bc2c974e4b5d24839714a0063e611b002b5
ArthurHallack <ArthurHallack2@gmail.com>
2024-07-07 17:47:15 -0300

    Yoshaaa

--- React-test/src/routes/Cadastrar.jsx
+++ React-test/src/routes/Cadastrar.jsx
@@ -11,10 +11,11 @@ const StyleCadastro = {
 
 function SignIn() {
     const RefCadastro1 = useRef()//Email
-    const RefCadastro2 = useRef()
-    const RefCadastro3 = useRef()
+    const RefCadastro2 = useRef()//Senha
+    const RefCadastro3 = useRef()//Csenha
     const RefCadastro4 = useRef()//Nome
     const RefCadastro5 = useRef()//Celular
+    const RefCadastro6 = useRef()//Situação
     const navigate = useNavigate()
 
     const Gravar = (e) => {
@@ -39,16 +40,21 @@ function SignIn() {
             <form id="Form-Cadastro">
                 <img src={engrenagemImg} alt="engrenagem"  id="IMG-cadastro" style={StyleCadastro}/>
                 <div id="Conteudo-Cadastro">
+                    <div id="BtnsTopCadastro">
+                        <span id="BtnPF">Pessoa Fisica</span>
+                        <span id="BtnPJ">Pessoa Juridica</span>
+                        <h1>Pessoa Fisica</h1>
+                    </div>
                     <div className="Single-input">
                         <input type="text" ref={RefCadastro1} className="inputCadastro" id="Email" required/>
                         <label for="Email">Email</label>
                     </div>
                     <div className="Single-input">
-                        <input type="text" ref={RefCadastro1} className="inputCadastro" id="Nome" required/>
+                        <input type="text" ref={RefCadastro4} className="inputCadastro" id="Nome" required/>
                         <label for="Nome">Nome</label>
                     </div>
                     <div className="Single-input">
-                        <input type="tel" ref={RefCadastro1} className="inputCadastro" id="Celular" required/>
+                        <input type="tel" ref={RefCadastro5} className="inputCadastro" id="Celular" required/>
                         <label for="Celular">Celular</label>
                     </div>
                     <div className="Single-input">
@@ -59,11 +65,13 @@ function SignIn() {
                         <input type="password" ref={RefCadastro3} className="inputCadastro" id="Csenha" required/>
                         <label for="Csenha">Confirme sua Senha</label>
                     </div>
-                    <div className="Single-input">
-                        <input type="checkbox" ref={RefCadastro1} className="inputCadastro" id="Sit" required/>
-                        <label for="Sit">Situação</label>
+                    <div className="Single-input" id="DivInputSit">
+                        <input type="checkbox" ref={RefCadastro6} className="inputCadastro" id="Sit" required/>
+                        <label for="Sit" id="labelSit">Situação</label>
                     </div>
-                    <button type="submit" onClick={Gravar} id="cadastroBTN">Cadastrar</button>
+                    <div id="DivCadastroBTN">
+                        <button type="submit" onClick={Gravar} id="cadastroBTN">Cadastrar</button>
+                    </div>
                 </div>
             </form>
         </div>
