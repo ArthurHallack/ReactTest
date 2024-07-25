2c7576ada2bc1c8d755eaac9a54dc873ec122707
Arthur <ArthurHallack2@gmail.com>
2024-07-25 18:34:22 -0300

    Yoshaaa

--- React-test/src/routes/CadastroPf.jsx
+++ React-test/src/routes/CadastroPf.jsx
@@ -19,100 +19,106 @@ function PFCadastro () {
                     <button><FontAwesomeIcon icon={faFilter} /></button>
                 </div>
             </div>
-            <form id="Form-DadosPessoais">
-                <div>
-                    <p><i>Selecionar Informações</i></p>
+            <div id="InfoAreaPF">
+                <form id="Form-DadosPessoais">
+                    <div>
+                        <p><i>Selecionar Informações</i></p>
 
-                </div>
-                <div id="CamposFormPF-DP">
-                    <div id="CamposNomePF-DP">
-                        <fieldset className="FieldDadosPessoais nomesPF-DP">
-                            <label>Nome Completo</label>
-                            <input type="text" />
-                        </fieldset>
-                        <fieldset className="FieldDadosPessoais nomesPF-DP">
-                            <label>Nome p/ reserva</label>
-                            <input type="text" />
-                        </fieldset >
-                        <fieldset className="FieldDadosPessoais nomesPF-DP">
-                            <label>Nome p/ crachá</label>
-                            <input type="text" />
-                        </fieldset>
                     </div>
-                    <div id="CamposOutrosPF-DP">
-                        <fieldset className="FieldDadosPessoais outrosPF-DP"> 
-                            <label>RG</label>
-                            <input type="text" />
-                        </fieldset>
-                        <fieldset className="FieldDadosPessoais outrosPF-DP">
-                            <label>CPF</label>
-                            <input type="text" />
-                        </fieldset>
-                        <fieldset className="FieldDadosPessoais outrosPF-DP">
-                            <label>Nacionalidade</label>
-                            <input type="text" />
-                        </fieldset>
+                    <div id="CamposFormPF-DP">
+                        <div id="CamposNomePF-DP">
+                            <fieldset className="FieldDadosPessoais nomesPF-DP">
+                                <label>Nome Completo</label>
+                                <input type="text" />
+                            </fieldset>
+                            <fieldset className="FieldDadosPessoais nomesPF-DP">
+                                <label>Nome p/ reserva</label>
+                                <input type="text" />
+                            </fieldset >
+                            <fieldset className="FieldDadosPessoais nomesPF-DP">
+                                <label>Nome p/ crachá</label>
+                                <input type="text" />
+                            </fieldset>
+                        </div>
+                        <div id="CamposOutrosPF-DP">
+                            <fieldset className="FieldDadosPessoais outrosPF-DP"> 
+                                <label>RG</label>
+                                <input type="text" />
+                            </fieldset>
+                            <fieldset className="FieldDadosPessoais outrosPF-DP">
+                                <label>CPF</label>
+                                <input type="text" />
+                            </fieldset>
+                            <fieldset className="FieldDadosPessoais outrosPF-DP">
+                                <label>Nacionalidade</label>
+                                <input type="text" />
+                            </fieldset>
+                        </div>
+                        <div id="CamposMenoresPF-DP">
+                            <fieldset className="FieldDadosPessoais menoresPF-DP">
+                                <label>Data de Nascimento</label>
+                                <input type="date" id="DataInputPF-DP"/>
+                            </fieldset>
+                            <fieldset className="FieldDadosPessoais menoresPF-DP">
+                                <label>Estado Civil</label>
+                                <select name="Situacao" id="OpCivilPF">
+                                        <option value="">Selecionar</option>
+                                        <option value="1">SOLTEIRO</option>
+                                        <option value="2">CASADO</option>
+                                        <option value="3">SEPARADO</option>
+                                        <option value="4">DIVORCIADO</option>
+                                        <option value="5">VIÚVO</option>
+                                </select>
+                            </fieldset>
+                            <fieldset className="FieldDadosPessoais menoresPF-DP">
+                                <label>Gênero</label>
+                                <select name="Situacao" id="OpGeneroPF">
+                                        <option value="">Selecionar</option>
+                                        <option value="1">MASCULINO</option>
+                                        <option value="2">FEMININO</option>
+                                        <option value="3">OUTROS</option>
+                                </select>
+                            </fieldset>
+                            <fieldset className="CheckPF-DP">
+                                <label>Fornecedor</label>
+                                <div>
+                                    <input type="checkbox" />
+                                    <p>Sim</p>
+                                </div>                            
+                            </fieldset>
+                            <fieldset className="CheckPF-DP">
+                                <label>Estrangeira</label>
+                                <div>
+                                    <input type="checkbox" />
+                                    <p>Sim</p>
+                                </div>
+                            </fieldset>
+                            <fieldset className="CheckPF-DP">
+                                <label>Notificação</label>
+                                <div>
+                                    <input type="checkbox" />
+                                    <p>Sim, receber</p>
+                                </div>
+                            </fieldset>
+                            <fieldset className="CheckPF-DP">
+                                <label>Situação</label>
+                                <div>
+                                    <input type="checkbox" />
+                                    <p>Ativo</p>
+                                </div>
+                            </fieldset>
+                        </div>
                     </div>
-                    <div id="CamposMenoresPF-DP">
-                        <fieldset className="FieldDadosPessoais menoresPF-DP">
-                            <label>Data de Nascimento</label>
-                            <input type="date" id="DataInputPF-DP"/>
-                        </fieldset>
-                        <fieldset className="FieldDadosPessoais menoresPF-DP">
-                            <label>Estado Civil</label>
-                            <select name="Situacao" id="OpCivilPF">
-                                    <option value="">Selecionar</option>
-                                    <option value="1">SOLTEIRO</option>
-                                    <option value="2">CASADO</option>
-                                    <option value="3">SEPARADO</option>
-                                    <option value="4">DIVORCIADO</option>
-                                    <option value="5">VIÚVO</option>
-                            </select>
-                        </fieldset>
-                        <fieldset className="FieldDadosPessoais menoresPF-DP">
-                            <label>Gênero</label>
-                            <select name="Situacao" id="OpGeneroPF">
-                                    <option value="">Selecionar</option>
-                                    <option value="1">MASCULINO</option>
-                                    <option value="2">FEMININO</option>
-                                    <option value="3">OUTROS</option>
-                            </select>
-                        </fieldset>
-                        <fieldset className="CheckPF-DP">
-                            <label>Fornecedor</label>
-                            <div>
-                                <input type="checkbox" />
-                                <p>Sim</p>
-                            </div>                            
-                        </fieldset>
-                        <fieldset className="CheckPF-DP">
-                            <label>Estrangeira</label>
-                            <div>
-                                <input type="checkbox" />
-                                <p>Sim</p>
-                            </div>
-                        </fieldset>
-                        <fieldset className="CheckPF-DP">
-                            <label>Notificação</label>
-                            <div>
-                                <input type="checkbox" />
-                                <p>Sim, receber</p>
-                            </div>
-                        </fieldset>
-                        <fieldset className="CheckPF-DP">
-                            <label>Situação</label>
-                            <div>
-                                <input type="checkbox" />
-                                <p>Ativo</p>
-                            </div>
-                        </fieldset>
+                    <div id="BTNsDadosPessoais">
+                        <button><FontAwesomeIcon icon={faCheck} /></button>
+                        <button><FontAwesomeIcon icon={faXmark}/></button>
                     </div>
+                </form>
+                <div id="AreaImgPF">
+                    <img src="" alt="" />
+                    <input type="file" placeholder="" />
                 </div>
-                <div id="BTNsDadosPessoais">
-                    <button><FontAwesomeIcon icon={faCheck} /></button>
-                    <button><FontAwesomeIcon icon={faXmark}/></button>
-                </div>
-            </form>
+            </div>
             <form id="Form-FilterPF">
                 <p><i>Selecionar Informações</i></p>
                 <div id="DivFiltroPF">
@@ -145,6 +151,15 @@ function PFCadastro () {
                     </div>
                 </div>
             </form>
+            <div id="ContedeuListPF">
+                <div id="HudConteudoPF">
+                    <ul>
+                        <li>ID</li>
+                        <li>Nome Completo</li>
+                        <li id="liNomeReserva">Nome para reservas</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
